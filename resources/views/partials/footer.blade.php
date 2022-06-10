44a94033c6157c6f263ecec379c1ce6f75e2917a
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-06-10 17:21:05 +0200

    main completato

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,21 +2,42 @@
     <div class="footer-top">
         <ul>
             <li><h3>Dc comics</h3></li>
-            <li></li>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
             <ul class="shop">
             <li><h3>Shop</h3></li>
-            <li></li>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
         </ul>
          </ul>
 
         <ul>
             <li><h3>Dc</h3></li>
-            <li></li>
+            <li>Terms of Use</li>
+            <li>Privacy Policy(New)</li>
+            <li>Ad choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent Workshops</li>
+            <li>CPSC Certificates</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Contact Us</li>
         </ul>
 
         <ul>
             <li><h3>Sites</h3></li>
-            <li></li>
+            <li>DC</li>
+            <li>MAD Magazine</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
         </ul>
         <img src="../img/dc-logo-bg.png" alt="">
     </div>
@@ -28,7 +49,19 @@
             <h4>Follow us</h4>
             <ul>
                 <li>
-                    <img :src="" alt="">
+                    <img src="../img/footer-facebook.png" alt="">
+                </li>
+                <li>
+                    <img src="../img/footer-twitter.png" alt="">
+                </li>
+                <li>
+                    <img src="../img/footer-youtube.png" alt="">
+                </li>
+                <li>
+                    <img src="../img/footer-pinterest.png" alt="">
+                </li>
+                <li>
+                    <img src="../img/footer-periscope.png" alt="">
                 </li>
             </ul>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comic = config('comics');
+    return view('home',['comic'=> $comic]);
 });
 
 Route::get('/shop', function () {
