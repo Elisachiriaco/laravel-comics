954ccabf8dcf2dc1dc1f30213b0ca62fd92ac8ca
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-06-10 16:14:47 +0200

    nav bar completata

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base')
+@section('pageTitle')
+    La Molisana 
+@endsection
+@section('mainContent')
+    <h1>Home</h1>
+    {{-- @dump($dati)
+    <p>{{ $dati['nome']}}</p> --}}
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,36 @@
+<footer>
+    <div class="footer-top">
+        <ul>
+            <li><h3>Dc comics</h3></li>
+            <li></li>
+            <ul class="shop">
+            <li><h3>Shop</h3></li>
+            <li></li>
+        </ul>
+         </ul>
+
+        <ul>
+            <li><h3>Dc</h3></li>
+            <li></li>
+        </ul>
+
+        <ul>
+            <li><h3>Sites</h3></li>
+            <li></li>
+        </ul>
+        <img src="../img/dc-logo-bg.png" alt="">
+    </div>
+    <div class="footer-down">
+        <div class="btn">
+            <a href="#">Sign-up now!</a>
+        </div>
+        <div class="icons">
+            <h4>Follow us</h4>
+            <ul>
+                <li>
+                    <img :src="" alt="">
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
